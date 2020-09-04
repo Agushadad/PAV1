bb0a2ab543afdcb9db27de8aa344a097694d47e6
Agustín Hadad <agushadad@gmail.com>
2020-09-04 15:22:12 -0300

    Modificacion empleados

--- Codigo/PAV1 TP/Formularios/Empleados/Modificacion_Empleado.Designer.cs
+++ Codigo/PAV1 TP/Formularios/Empleados/Modificacion_Empleado.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PAV1_TP.Formularios.Empleados
 {
-    partial class Alta_Empleado
+    partial class Modificacion_Empleado
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Alta_Empleado));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Modificacion_Empleado));
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -49,233 +50,212 @@
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(119, 212);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(191, 21);
+            this.comboBox3.TabIndex = 78;
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(110, 246);
+            this.comboBox2.Location = new System.Drawing.Point(119, 246);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(191, 21);
-            this.comboBox2.TabIndex = 56;
-            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            this.comboBox2.TabIndex = 77;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(13, 175);
+            this.label5.Location = new System.Drawing.Point(22, 175);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(141, 20);
-            this.label5.TabIndex = 54;
+            this.label5.TabIndex = 76;
             this.label5.Text = "Datos de Domicilio";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(13, 60);
+            this.label4.Location = new System.Drawing.Point(22, 60);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(134, 20);
-            this.label4.TabIndex = 53;
+            this.label4.TabIndex = 75;
             this.label4.Text = "Datos personales";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(3, 9);
+            this.label3.Location = new System.Drawing.Point(12, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(198, 24);
-            this.label3.TabIndex = 52;
-            this.label3.Text = "Registro Empleados";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label3.Size = new System.Drawing.Size(194, 24);
+            this.label3.TabIndex = 74;
+            this.label3.Text = "Modificar empleado";
             // 
             // label11
             // 
             this.label11.AutoSize = true;
             this.label11.BackColor = System.Drawing.Color.Transparent;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label11.Location = new System.Drawing.Point(42, 320);
+            this.label11.Location = new System.Drawing.Point(51, 320);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(64, 16);
-            this.label11.TabIndex = 50;
+            this.label11.TabIndex = 72;
             this.label11.Text = "N° Calle";
-            this.label11.Click += new System.EventHandler(this.label11_Click);
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.BackColor = System.Drawing.Color.Transparent;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(60, 282);
+            this.label10.Location = new System.Drawing.Point(69, 282);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(44, 16);
-            this.label10.TabIndex = 49;
+            this.label10.TabIndex = 71;
             this.label10.Text = "Calle";
-            this.label10.Click += new System.EventHandler(this.label10_Click);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
             this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(54, 247);
+            this.label9.Location = new System.Drawing.Point(63, 247);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(50, 16);
-            this.label9.TabIndex = 47;
+            this.label9.TabIndex = 70;
             this.label9.Text = "Barrio";
-            this.label9.Click += new System.EventHandler(this.label9_Click);
             // 
             // label12
             // 
             this.label12.AutoSize = true;
             this.label12.BackColor = System.Drawing.Color.Transparent;
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(331, 215);
+            this.label12.Location = new System.Drawing.Point(340, 215);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(70, 16);
-            this.label12.TabIndex = 46;
+            this.label12.TabIndex = 69;
             this.label12.Text = "Telefono";
-            this.label12.Click += new System.EventHandler(this.label12_Click);
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(27, 213);
+            this.label8.Location = new System.Drawing.Point(36, 213);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(77, 16);
-            this.label8.TabIndex = 45;
+            this.label8.TabIndex = 68;
             this.label8.Text = "Localidad";
-            this.label8.Click += new System.EventHandler(this.label8_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(40, 141);
+            this.label2.Location = new System.Drawing.Point(49, 141);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(66, 16);
-            this.label2.TabIndex = 51;
+            this.label2.TabIndex = 73;
             this.label2.Text = "Apellido";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(380, 107);
+            this.label7.Location = new System.Drawing.Point(389, 107);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(21, 16);
-            this.label7.TabIndex = 44;
+            this.label7.TabIndex = 67;
             this.label7.Text = "Id";
-            this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(41, 105);
+            this.label1.Location = new System.Drawing.Point(50, 105);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(63, 16);
-            this.label1.TabIndex = 42;
+            this.label1.TabIndex = 66;
             this.label1.Text = "Nombre";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // btn_CancelarEmpleado
             // 
-            this.btn_CancelarEmpleado.Location = new System.Drawing.Point(534, 354);
+            this.btn_CancelarEmpleado.Location = new System.Drawing.Point(543, 354);
             this.btn_CancelarEmpleado.Name = "btn_CancelarEmpleado";
             this.btn_CancelarEmpleado.Size = new System.Drawing.Size(82, 23);
-            this.btn_CancelarEmpleado.TabIndex = 41;
+            this.btn_CancelarEmpleado.TabIndex = 65;
             this.btn_CancelarEmpleado.Text = "Cancelar";
             this.btn_CancelarEmpleado.UseVisualStyleBackColor = true;
-            this.btn_CancelarEmpleado.Click += new System.EventHandler(this.button3_Click);
             // 
             // btn_RegistrarEmpleado
             // 
-            this.btn_RegistrarEmpleado.Location = new System.Drawing.Point(453, 354);
+            this.btn_RegistrarEmpleado.Location = new System.Drawing.Point(462, 354);
             this.btn_RegistrarEmpleado.Name = "btn_RegistrarEmpleado";
             this.btn_RegistrarEmpleado.Size = new System.Drawing.Size(75, 23);
-            this.btn_RegistrarEmpleado.TabIndex = 40;
+            this.btn_RegistrarEmpleado.TabIndex = 64;
             this.btn_RegistrarEmpleado.Text = "Registrar";
             this.btn_RegistrarEmpleado.UseVisualStyleBackColor = true;
-            this.btn_RegistrarEmpleado.Click += new System.EventHandler(this.button2_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(110, 104);
+            this.textBox2.Location = new System.Drawing.Point(119, 104);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(191, 20);
-            this.textBox2.TabIndex = 38;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.TabIndex = 63;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(418, 107);
+            this.textBox4.Location = new System.Drawing.Point(427, 107);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(84, 20);
-            this.textBox4.TabIndex = 37;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            this.textBox4.TabIndex = 62;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(110, 319);
+            this.textBox7.Location = new System.Drawing.Point(119, 319);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(191, 20);
-            this.textBox7.TabIndex = 36;
-            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            this.textBox7.TabIndex = 61;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(110, 281);
+            this.textBox6.Location = new System.Drawing.Point(119, 281);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(191, 20);
-            this.textBox6.TabIndex = 35;
-            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            this.textBox6.TabIndex = 60;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(407, 214);
+            this.textBox8.Location = new System.Drawing.Point(416, 214);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(209, 20);
-            this.textBox8.TabIndex = 33;
-            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
+            this.textBox8.TabIndex = 59;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(110, 137);
+            this.textBox1.Location = new System.Drawing.Point(119, 137);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(191, 20);
-            this.textBox1.TabIndex = 32;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.TabIndex = 58;
             // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(110, 212);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(191, 21);
-            this.comboBox3.TabIndex = 57;
-            // 
-            // Alta_Empleado
+            // Modificacion_Empleado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(634, 393);
+            this.ClientSize = new System.Drawing.Size(641, 391);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.comboBox2);
             this.Controls.Add(this.label5);
@@ -298,8 +278,8 @@
             this.Controls.Add(this.textBox8);
             this.Controls.Add(this.textBox1);
             this.MaximizeBox = false;
-            this.Name = "Alta_Empleado";
-            this.Text = "Registro de Empleados";
+            this.Name = "Modificacion_Empleado";
+            this.Text = "Modificacion_Empleado";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -307,6 +287,7 @@
 
         #endregion
 
+        private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
@@ -327,6 +308,5 @@
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
---
+++ Codigo/PAV1 TP/Formularios/Empleados/Modificacion_Empleado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PAV1_TP.Formularios.Empleados
+{
+    public partial class Modificacion_Empleado : Form
+    {
+        public Modificacion_Empleado()
+        {
+            InitializeComponent();
+        }
+    }
+}
